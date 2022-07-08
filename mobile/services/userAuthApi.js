da0f77e9c6dacfe9b2f257d9dbd31e31404c0577
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2022-07-08 07:52:48 +0200

    fixing errors

--- mobile/services/userAuthApi.js
+++ mobile/services/userAuthApi.js
@@ -3,11 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const userAuthApi = createApi({
   reducerPath: 'userAuthApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api/v1/user/'
+    baseUrl: 'http://192.168.0.221:8000/api/v1/user/'
   }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (user) => {
+        console.log(user)
         return {
           url: 'register',
           method: 'POST',
